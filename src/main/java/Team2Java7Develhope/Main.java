1016d15a2719975bfa5810a7122bd75ce5ea0be7
Kram037 <defenzomarco@gmail.com>
2023-02-03 15:36:10 +0100

    Added Spring boot tools

--- src/main/java/Team2Java7Develhope/Main.java
+++
@@ -1,7 +0,0 @@
-package Team2Java7Develhope;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
---
+++ src/main/java/co/gruppo2/studiomedico/StudiomedicoApplication.java
@@ -0,0 +1,13 @@
+package co.gruppo2.studiomedico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudiomedicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudiomedicoApplication.class, args);
+	}
+
+}
