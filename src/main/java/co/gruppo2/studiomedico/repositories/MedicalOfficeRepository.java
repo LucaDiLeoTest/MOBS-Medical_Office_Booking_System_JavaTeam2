baddaa803a9f3015c0ac969005c8110b245c363c
Luca Di Leo <lucadileo992@gmail.com>
2023-02-17 10:49:01 +0100

    feat: add booking feature,fix minor connection issues

--- src/main/java/co/gruppo2/studiomedico/repositories/MedicalOfficeRepository.java
+++
@@ -1,8 +0,0 @@
-package co.gruppo2.studiomedico.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MedicalOfficeRepository extends JpaRepository {
-}
