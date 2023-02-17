c607d6e7670894bde45a83a8409fa86a0ff8eff2
Nicola <nicolaboniello88@gmail.com>
2023-02-17 19:57:41 +0100

    minor fix ReceptionistRepository

--- src/main/java/co/gruppo2/studiomedico/repositories/IReceptionistRepository.java
+++ src/main/java/co/gruppo2/studiomedico/repositories/IReceptionistRepository.java
@@ -2,6 +2,8 @@ package co.gruppo2.studiomedico.repositories;
 
 import co.gruppo2.studiomedico.entities.Receptionist;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface IReceptionistRepository extends JpaRepository<Receptionist, Long> {
 }
